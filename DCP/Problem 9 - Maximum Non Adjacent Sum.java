9f512ce56d592cf3736bbf12836ba52e22c27c94
Harish Veeramani <hdv987@gmail.com>
2018-02-13 22:49:38 -0600

    Add solution to problem 9

--- DCP/Problem 9 - Maximum Non Adjacent Sum.java
+++ DCP/Problem 9 - Maximum Non Adjacent Sum.java
@@ -12,20 +12,31 @@ Also on LeetCode:
 
 */
 
-//Initial attempt (NOT WORKING))
+//First variation solution
+//O(n) time complexity
+//O(n) space complexity
 public int maxSumNonAdjacent(int[] nums) {
-	if(nums == null || nums.length == 0){
-		return 0;
-	}
-		
-	int[] maxSum = new int[nums.length + 1];
-	maxSum[0] = 0;
-	maxSum[1] = nums[0];
-	
-	for (int i = 1; i < nums.length; i++){
-		int add = Math.max(maxSum[i - 1], maxSum[i]);
-		maxSum[i + 1] += nums[i] + maxSum[add];
-	}
-	
-	return maxSum[nums.length];
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        if (nums.length == 1){
+            return nums[0];
+        }
+            
+        int[] maxSum = new int[nums.length + 1];
+        maxSum[0] = 0;
+        maxSum[1] = nums[0];
+        maxSum[2] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++){
+            int add = Math.max(maxSum[i - 1], maxSum[i - 2]) == maxSum[i - 1] ? i - 1: i - 2; 
+            if (nums[i] > 0){
+                maxSum[i + 1] += nums[i] + maxSum[add];
+            } else {
+                maxSum[i + 1] = maxSum[add];
+            }
+        }
+        
+        return Math.max(maxSum[nums.length], maxSum[nums.length - 1]);
 }
