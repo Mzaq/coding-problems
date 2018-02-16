ab9730df5993206ef3d38a28e2c0fd9dbcf5aa6b
Harish Veeramani <hdv987@gmail.com>
2018-02-15 23:59:41 -0600

    Add O(1) space solution

--- DCP/Problem 12 - Climbing Stairs.java
+++ DCP/Problem 12 - Climbing Stairs.java
@@ -30,3 +30,20 @@ public int climbStairs(int n) {
 	
 	return numWays[n];
 }
+
+//O(n) time, O(1) space solution
+public int climbStairs(int n) {
+	if (n <= 0) return 0;
+	if (n == 1) return 1;
+	
+	int prev = 1;
+	int curr = 2;
+
+	for (int i = 2; i < n; i++){
+		int temp = curr;
+		curr = curr + prev;
+		prev = temp;
+	}
+	
+	return curr;
+}
