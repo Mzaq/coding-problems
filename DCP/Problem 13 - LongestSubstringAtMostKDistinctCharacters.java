c4fe3d2341b9dcccdc25a583aedc975947178187
Harish Veeramani <hdv987@gmail.com>
2018-02-21 23:39:53 -0600

    Add cleaner solution for problem 13

--- DCP/Problem 13 - LongestSubstringAtMostKDistinctCharacters.java
+++ DCP/Problem 13 - LongestSubstringAtMostKDistinctCharacters.java
@@ -7,31 +7,26 @@ For example, given s = "abcba" and k = 2, the longest substring with k distinct 
 */
 
 
-//Initial O(n) time/space solution w/ HashMap
+//O(n) time, O(k) space solution w/ HashMap
 public int lengthOfLongestSubstringKDistinct(String s, int k) {
 	if (s.equals("") || k == 0) return 0;
-	
-	int longestSubstring = 0;
-	char[] string = s.toCharArray();
-	Map<Character, Integer> charCount = new HashMap<>();
-	int left = 0;
-	int right = 0;
-	
-	while (right < string.length){
-		int count = charCount.getOrDefault(string[right], 0);
-		charCount.put(string[right], ++count);
-		longestSubstring = Math.max(longestSubstring, right - left + 1);
 
-		if (charCount.size() == k && right < s.length() - 1 && !charCount.containsKey(string[right + 1])){
-			while (charCount.size() == k){
-				charCount.put(string[left], charCount.get(string[left]) - 1);
-				if (charCount.get(string[left]) == 0){
-					charCount.remove(string[left]);
-				}
-				left++;
+	Map<Character, Integer> charCount = new HashMap<>();
+	int left = 0; int right = 0; int longestSubstring = 0;
+
+	while (right < s.length()){
+		char current = s.charAt(right++);
+		charCount.put(current, charCount.getOrDefault(current, 0) + 1);
+
+		while (charCount.size() > k){
+			char leftChar = s.charAt(left++);
+			charCount.put(leftChar, charCount.get(leftChar) - 1);
+			if (charCount.get(leftChar) == 0){
+				charCount.remove(leftChar);
 			}
 		}
-		right++;
+
+		longestSubstring = Math.max(longestSubstring, right - left);
 	}
 	
 	return longestSubstring;
