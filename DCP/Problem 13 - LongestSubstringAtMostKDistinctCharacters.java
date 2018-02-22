3812be45f1611b7d6e5d33984348bc02124b315c
Harish Veeramani <hdv987@gmail.com>
2018-02-21 23:41:32 -0600

    Add LeetCode links to problem 13

--- DCP/Problem 13 - LongestSubstringAtMostKDistinctCharacters.java
+++ DCP/Problem 13 - LongestSubstringAtMostKDistinctCharacters.java
@@ -4,6 +4,10 @@ This problem was asked by Amazon and Google.
 Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
 
 For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb", so your function should return 3.
+
+Also on LeetCode: 
+- https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters
+- https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters
 */
 
 
