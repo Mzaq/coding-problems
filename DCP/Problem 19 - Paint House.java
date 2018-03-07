d1cebd93b5e51e430a1d8a93a7d4c07d72d6ad5c
Harish Veeramani <hdv987@gmail.com>
2018-03-06 22:53:01 -0600

    Add 2nd part of problem 19

--- DCP/Problem 19 - Paint House.java
+++ DCP/Problem 19 - Paint House.java
@@ -29,3 +29,38 @@ public int minCost(int[][] costs) {
 	int size = minCost.length;
 	return Math.min(Math.min(minCost[size - 1][0], minCost[size - 1][1]), minCost[size - 1][2]);
 }
+
+//Paint House II O(n*k*k) 
+Integer[][] minCost;
+
+public int minCostII(int[][] costs) {
+	if (costs == null || costs.length == 0) return 0;
+	if (costs[0].length == 1) return costs[0][0];
+
+	minCost = new Integer[costs.length + 1][costs[0].length];
+	for (int i = 0; i < minCost[0].length; i++){
+		minCost[0][i] = 0;
+	}
+
+	for (int i = 1; i < minCost.length; i++){
+		for (int k = 0; k < costs[0].length; k++){
+			minCost[i][k] = minCost(i - 1, k) + costs[i-1][k];
+		}
+	}
+
+	return Collections.min(Arrays.asList(minCost[costs.length]));
+}
+
+private int minCost(int house, int color){
+	int min = Integer.MAX_VALUE;
+	
+	for (int k = 0; k < minCost[0].length; k++){
+		if (k != color && minCost[house][k] < min){
+			min = minCost[house][k];
+		}
+	}
+	
+	return min;
+}
+
+//Can be done in O(n*k)
