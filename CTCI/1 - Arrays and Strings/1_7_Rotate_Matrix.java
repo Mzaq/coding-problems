f64a61fa2841780edc89801e4b148f71ca6b82f2
Harish Veeramani <hdv987@gmail.com>
2018-05-28 18:02:00 -0400

    Add leetcode link and title

--- CTCI/1 - Arrays and Strings/1_7_Rotate_Matrix.java
+++ CTCI/1 - Arrays and Strings/1_7_Rotate_Matrix.java
@@ -1,6 +1,11 @@
 /*
+1.7 - Rotate Matrix
+
 Given an image represented by an NxN matrix, where each pixel in the image is 4
 bytes, write a method to rotate the image by 90 degrees. (an you do this in place?
+
+Also on LeetCode: https://leetcode.com/problems/rotate-image/description/
+
 */
 
 public int[][] rotateMatrix(int[][] matrix) {
