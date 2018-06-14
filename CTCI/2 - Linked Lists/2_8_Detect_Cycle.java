2d36ccbb2bc6a41cda2ec4ac8413171c3e015e5f
Harish Veeramani <hdv987@gmail.com>
2018-06-14 00:18:39 -0400

    Add optimal solution problem 2.8

--- CTCI/2 - Linked Lists/2_8_Detect_Cycle.java
+++ CTCI/2 - Linked Lists/2_8_Detect_Cycle.java
@@ -26,4 +26,30 @@ public ListNode detectCycle(ListNode head) {
 	return null;
 }
 
-//TODO: O(1) space solution
+//O(1) space solution
+public ListNode detectCycle(ListNode head) {
+	ListNode slow = head;
+	ListNode fast = head;
+	boolean cycle = false;
+	
+	while (fast != null && fast.next != null) {
+		slow = slow.next;
+		fast = fast.next.next;
+		if (slow == fast) {
+			cycle = true;
+			break;
+		}
+	}
+	if (!cycle) {
+		return null;
+	}
+	
+	fast = head;
+	
+	while (fast != slow) {
+		fast = fast.next;
+		slow = slow.next;
+	}
+	
+	return slow;
+}
