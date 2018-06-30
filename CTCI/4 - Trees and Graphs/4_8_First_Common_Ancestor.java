c514d00fc7667b87c544add249c3d698f179e096
Harish Veeramani <hdv987@gmail.com>
2018-06-30 17:52:45 -0400

    Add binary tree solutuion 4.8

--- CTCI/4 - Trees and Graphs/4_8_First_Common_Ancestor.java
+++ CTCI/4 - Trees and Graphs/4_8_First_Common_Ancestor.java
@@ -59,3 +59,44 @@ public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 	
 	return current;
 }
+
+//Binary tree version first attempt
+boolean found = false;
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	Deque<TreeNode> stackP = new LinkedList<>();
+	Deque<TreeNode> stackQ = new LinkedList<>();
+	findNode(stackP, root, p);
+	found = false;
+	findNode(stackQ, root, q);
+
+	TreeNode LCA = stackP.size() > stackQ.size() ? findLCA(stackP, stackQ) : findLCA(stackQ, stackP);
+	return LCA;
+
+}
+
+private void findNode(Deque<TreeNode> stack, TreeNode root, TreeNode target) {
+	if (root == null) return;
+	
+	TreeNode current = root;
+	stack.addFirst(current);
+	
+	if (current.val == target.val) found = true;
+	
+	if (!found) findNode(stack, root.left, target);
+	if (!found) findNode(stack, root.right, target);
+	
+	if (found) return;
+	else stack.removeFirst();
+}
+
+private TreeNode findLCA(Deque<TreeNode> stack1, Deque<TreeNode> stack2) {
+	while (stack1.size() > stack2.size()) stack1.removeFirst();
+	TreeNode curr1 = stack1.peekFirst();
+	TreeNode curr2 = stack2.peekFirst();
+	while (curr1.val != curr2.val) {
+		curr1 = stack1.removeFirst();
+		curr2 = stack2.removeFirst();
+	}
+	
+	return curr1;
+}
